fcf4fd316ffa5d71c39c32d2f8322ba243d0dcac
USP <USP@10.15.14.148>
2018-05-03 22:46:32 -0500

    Registro de notas

---
+++ index.php
@@ -0,0 +1,19 @@
+<body>
+ <h2>Registro de Notas</h2>
+ <form method="POST" action="kardex.php">
+ Nombre: <br />
+ <input type="text" name="nombre" size="50" />
+ <p>
+ Apellido: <br />
+ <input type="text" name="email" size="50" />
+ <p>
+ Nota 1: <br />
+ <input type="text" name="nota1" size="4" />
+ <p>
+ Nota 2: <br />
+ <input type="text" name="nota2" size="4" />
+  <p>
+ <input type="submit" name="submit" value="Registrar" />
+ <input type="reset" value="limpiar"  name="limpiar">
+ </form>
+ </body>
