bb16cb2a586b849563a92a6d9af142f2ffcdfc84
USP <USP@10.15.14.148>
2018-05-03 22:47:31 -0500

    Kardex de notas

---
+++ kardex.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <h1 align="center">Kardex de Alumnos</h1>
+    <table width="70%" border="1px" align="center">
+
+    <tr align="center">
+        <td>Nombre</td>
+        <td>Apellido</td>
+        <td>Nota 1</td>
+        <td>Nota 2</td>
+        <td>Promedio</td>
+   
+    </tr>
+    <tr align="center">
+        <td> Jostein</td>
+        <td> Huerta</td>
+        <td> 16</td>
+        <td> 14</td>
+        <td> 15</td>
+    </tr>
+   
